27e0defe2e3f6139322ba60a1612c397234226fc
Anastasiya Penya <53582712+CurlyAnastasi@users.noreply.github.com>
2020-10-03 13:52:12 +0000

    docs: add functions min, max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  const reducer = (previousValue, currentValue) => {
+    if (isNaN(previousValue) || isNaN(currentValue)) {
+      return 0;
+    } else {
+      return previousValue <= currentValue ? previousValue : currentValue;
+    }
+  }
+  return array == null || array.length == 0 ? 0 : array.reduce(reducer);
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+  const reducer = (previousValue, currentValue) => {
+    if (isNaN(previousValue) || isNaN(currentValue)) {
+      return 0;
+    } else {
+      return previousValue >= currentValue ? previousValue : currentValue;
+    }
+  }
+
+  return array == null || array.length === 0 ? 0 : array.reduce(reducer);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let sum;
+  let avg;
+
+  const reducer = (previousValue, currentValue) => {
+    if (isNaN(previousValue) || isNaN(currentValue)) {
+      return 0;
+    } else {
+      return previousValue + currentValue;
+    }
+  }
+
+  sum = array == null || array.length == 0 ? 0 : array.reduce(reducer);
+  avg = sum === 0 ? 0 : sum / array.length;
+
+  return avg;
 }
